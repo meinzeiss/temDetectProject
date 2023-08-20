5c067d21e16995693ac3b790ae7d79a9af546704
Jin Su <97459369+meinzeiss@users.noreply.github.com>
2023-08-20 12:15:50 -0300

    Auto-generated commit [ci skip]

--- src/app/task-directive.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TaskDirectiveDirective } from './task-directive.directive';
-
-describe('TaskDirectiveDirective', () => {
-  it('should create an instance', () => {
-    const directive = new TaskDirectiveDirective();
-    expect(directive).toBeTruthy();
-  });
-});
